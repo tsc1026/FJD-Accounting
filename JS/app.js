9e66161dccf9096572034c4ec867bed46f18d0b0
Melo <melomelo1988@gmail.com>
2021-01-18 16:43:41 +1100

    'uploadToGit'

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,26 @@
 const date = (document.getElementById(
     "date"
   ).innerHTML = new Date().getFullYear());
+
+const bookings = [];
+
+const createBooking = function(
+  flightNumber, 
+  numPassengers = 1, 
+  price = 199* numPassengers){
+
+    const booking = {
+      flightNumber,
+      numPassengers,
+      price
+    };
+
+    bookings.push(booking);
+    console.log(bookings);
+}
+
+createBooking('Melo-123');
+createBooking('Melo-123',3);
+createBooking('Melo-123', 2, 800);
+createBooking('Melo-123', undefined, 1000);
+
